905ae486ca99c80c49d3a071ad7b567c7310983c
JorgeHerter <your-github-email@example.com>
2025-04-16 13:21:05 -0600

    project update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      // Exclude test-related dependencies from the production build
+      external: ['jest'],
+    },
+  },
+  server: {
+    // Set the default port for the development server
+    port: 8080,
+    open: true, // Automatically open the browser when the server starts
+  },
+});
