8533e731a13bd264c2919b683b5d8411d6e23083
JorgeHerter <your-github-email@example.com>
2025-04-21 13:26:29 -0600

    project update

--- src/api.js
+++ src/api.js
@@ -189,7 +189,7 @@ if (code) {
 }*/
 
 import mockData from './mock-data';
-import { logout, startOAuthProcess } from './auth-service';
+//import { logout, startOAuthProcess } from './auth-service';
 import NProgress from 'nprogress';
 
 const API_BASE_URL = 'https://tlhsvksy0f.execute-api.us-east-1.amazonaws.com/dev';
